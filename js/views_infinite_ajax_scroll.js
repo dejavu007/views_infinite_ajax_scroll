de0f4d70c452a091e883aee7caf01176650dc814
Titto Jose <dejavu007@users.noreply.github.com>
2015-11-03 17:36:49 +0530

    Saving module development progress

--- js/views_infinite_ajax_scroll.js
+++ js/views_infinite_ajax_scroll.js
@@ -7,16 +7,16 @@
   Drupal.behaviors.views_ias = {
     attach: function (context, settings) {
 	  var iasViewsId = Drupal.settings.viewIas.id;
-	  var iasContainer = iasViewsId + Drupal.settings.viewIas.container;
-	  var iasItem = iasViewsId + Drupal.settings.viewIas.item;
-	  var iasPagination = iasViewsId + Drupal.settings.viewIas.pagination;
-	  var iasNext = iasViewsId + Drupal.settings.viewIas.next;
+	  var iasContainer = iasViewsId + ' ' + Drupal.settings.viewIas.container;
+	  var iasItem = iasViewsId + ' ' + Drupal.settings.viewIas.item;
+	  var iasPagination = iasViewsId + ' ' + Drupal.settings.viewIas.pagination;
+	  var iasNext = iasViewsId + ' ' + Drupal.settings.viewIas.next;
 	  var iasdelay = Drupal.settings.viewIas.delay; // default 500
 	  var iasNegMargin = Drupal.settings.viewIas.negativeMargin; // default 300
 
       var ias = $.ias({
         container:      iasContainer,
-        item:           '.views-row',
+        item:           iasItem,
         pagination:     iasPagination,
         next:           iasNext,
         delay:          iasdelay,
